a481387c441987e6dbbcf4f08079b461337f10df
annmargarettel <annmargarettel@gmail.com>
2026-09-12 01:56:18 +0800

    B Revert "feature/dashboard-compatible-mart"
    This reverts commit 345288ac5afe2d6f7a33e65e41f09993b2b37370.

--- src/sql/03_mart/04_dim_module_presentation.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE TABLE oulad.mart.dim_module_presentation AS
-SELECT DISTINCT
-    c.code_module,
-    dc.code_module AS code_presentation_module, -- validates reference to dim_course
-    c.code_presentation,
-    c.module_presentation_length
-FROM oulad.clean.courses AS c
-INNER JOIN oulad.mart.dim_course AS dc
-    ON c.code_module = dc.code_module;
