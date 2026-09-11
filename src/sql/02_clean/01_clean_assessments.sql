bbfc8797f2b401ae38f5829376942e607868467d
mia.barroga@ftwfoundation.org <mia.barroga@ftwfoundation.org>
2026-09-11 11:25:38 +0000

    Removed create temporary view

--- src/sql/02_clean/01_clean_assessments.sql
+++ src/sql/02_clean/01_clean_assessments.sql
@@ -8,19 +8,6 @@
   ============================================================================ */
 
 
-CREATE SCHEMA IF NOT EXISTS oulad.clean;
-
-
--- Load raw CSV as STRING so the literal text "null" doesn't break casting.
-CREATE OR REPLACE TEMPORARY VIEW raw_assessments AS
-SELECT code_module, code_presentation, id_assessment, assessment_type, date, weight
-FROM read_files(
- '/Volumes/workspace/default/ftw-b12/shared/week07/assessments.csv',
- format => 'csv',
- header => true,
- schema => 'code_module STRING, code_presentation STRING, id_assessment STRING, assessment_type STRING, date STRING, weight STRING'
-);
-
 
 CREATE OR REPLACE TABLE oulad.clean.clean_assessments AS
 SELECT
@@ -35,7 +22,8 @@ SELECT
  END                                                      AS assessment_type,
  try_cast(NULLIF(lower(trim(date)),   'null') AS INT)     AS date,               -- "null" text -> real NULL -> int
  try_cast(NULLIF(lower(trim(weight)), 'null') AS DOUBLE)  AS weight              -- "null" text -> real NULL -> double
-FROM raw_assessments
-;
+ 
+FROM oulad.raw.assessments;
+
 
 
