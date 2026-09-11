e0798f3b211c2f7ce7d3972c1ad6e6417f2f7ecb
anjelika.marquez@ftwfoundation.org <anjelika.marquez@ftwfoundation.org>
2026-09-11 01:18:05 +0000

    Updated code for vle silver
    create repalce table
    cast to trycast

--- src/sql/02_clean/07_clean_vle.sql
+++ src/sql/02_clean/07_clean_vle.sql
@@ -1,11 +1,10 @@
-CREATE TABLE oulad.clean.vle AS
-WITH data_cleaning AS (
-    SELECT
-        CAST(id_site AS INT) AS id_site,
-        UPPER(TRIM(code_module)) AS code_module,
-        UPPER(TRIM(code_presentation)) AS code_presentation,
-        LOWER(TRIM(activity_type)) AS activity_type,
-        CAST(week_from AS INT) AS week_from,
-        CAST(week_to AS INT) AS week_to
-    FROM oulad.raw.vle
-),
+CREATE OR REPLACE TABLE oulad.clean.vle AS
+SELECT
+    TRY_CAST(id_site AS INT) AS id_site,
+    UPPER(TRIM(code_module)) AS code_module,
+    UPPER(TRIM(code_presentation)) AS code_presentation,
+    LOWER(TRIM(activity_type)) AS activity_type,
+    TRY_CAST(week_from AS INT) AS week_from,
+    TRY_CAST(week_to AS INT) AS week_to
+FROM oulad.raw.vle
+
